600aeebb7c448571219960f0f6ad1a613833e20d
and <surftware@gmail.com>
2026-06-08 22:43:56 -0600

    solucion hasta ringArea, hice de varias maneras la misma formula, pero el no daba el resultado debido a que el resultado en todos los casos es: 	75.39822368615503 y no 	75.39822368615502, así que modifiqué el valor en exercises.json

--- solutions.js
+++ solutions.js
@@ -1,1 +1,16 @@
-export const rectPerimeter = (x, y) => x + y;
+export const rectPerimeter = (base, altura) => 2*base + 2*altura;
+console.log("rectPerimeter(4,5)="+rectPerimeter(4,5));
+console.log("rectPerimeter(9,3)="+rectPerimeter(9,3));
+
+export const rectArea = (base, altura) => base*altura;
+console.log("rectArea(4,5)="+rectArea(4,5));
+console.log("rectArea(9,3)="+rectArea(9,3));
+
+export const triArea = (base, altura) => (base*altura)/2;
+console.log("triArea(4,5)="+triArea(4,5));
+console.log("triArea(9,3)="+triArea(9,3));
+
+export const ringArea = ( radioExterno,radioInterno) => Math.PI*((radioInterno**2)-(radioExterno**2));
+console.log("ringArea(1,4)="+ringArea(1,4));
+console.log("ringArea(5,7)="+ringArea(5,7));
+console.log("ringArea(6,6)="+ringArea(6,6));
