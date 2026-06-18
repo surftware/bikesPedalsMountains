e6f7acfec5dd6e558b04938c5a7ff464363e59bf
and <surftware@gmail.com>
2026-06-18 06:31:26 -0600

    js y css de seccion de rules

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,29 @@ accordionTriggers.forEach((trigger) => {
         const panelId = trigger.getAttribute("aria-controls");
         const panel = document.getElementById(panelId);
 
-        const isExpanded = trigger.getAttribute("aria-expanded") === "true";
+        const isOpen =
+            trigger.getAttribute("aria-expanded") === "true";
 
-        trigger.setAttribute("aria-expanded", String(!isExpanded));
-        panel.hidden = isExpanded;
+        // Cerrar todos los paneles
+        accordionTriggers.forEach((otherTrigger) => {
+            const otherPanelId =
+                otherTrigger.getAttribute("aria-controls");
+
+            const otherPanel =
+                document.getElementById(otherPanelId);
+
+            otherTrigger.setAttribute("aria-expanded", "false");
+            otherTrigger.classList.remove("active");
+
+            otherPanel.hidden = true;
+        });
+
+        // Abrir el seleccionado solo si antes estaba cerrado
+        if (!isOpen) {
+            trigger.setAttribute("aria-expanded", "true");
+            trigger.classList.add("active");
+
+            panel.hidden = false;
+        }
     });
 });
